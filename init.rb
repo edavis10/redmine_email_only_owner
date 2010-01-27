238c4007765cb321c2f95ae9007ed64ace969e88
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-27 11:04:27 -0800

    Added project docs and test setup

--- init.rb
+++ init.rb
@@ -2,7 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_email_only_owner do
   name 'Redmine Email Only Owner plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'This plugin adds a Project module that will make issue events only send emails to the issue owner.'
+  version '0.1.0'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
+
+# Helpers
+class ActiveSupport::TestCase
+  def setup
+    User.anonymous
+    begin
+      Role.non_member
+    rescue
+      non_member = Role.generate!
+      non_member.builtin = Role::BUILTIN_NON_MEMBER
+      non_member.save!
+    end
+  end
+end
