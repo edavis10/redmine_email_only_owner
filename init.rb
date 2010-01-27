fb35bcbfec898b51326d67f439fce22909547b28
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-27 10:49:31 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_email_only_owner do
+  name 'Redmine Email Only Owner plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
