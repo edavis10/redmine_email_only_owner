017b44b861a5cdfd89453fe437d3ff6c11cb80c4
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-27 11:11:27 -0800

    [#3532] Added a project level module.

--- init.rb
+++ init.rb
@@ -7,4 +7,9 @@ Redmine::Plugin.register :redmine_email_only_owner do
   author_url 'http://www.littlestreamsoftware.com'
   description 'This plugin adds a Project module that will make issue events only send emails to the issue owner.'
   version '0.1.0'
+
+  project_module :email_only_owner do
+    # TODO: Redmine requires a permission to define a module
+    permission :email_only_owner, {}, :public => true
+  end
 end
